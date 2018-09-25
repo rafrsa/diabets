2f7115bcabb9d1695cd8a9781d8c3f4710a8a6df
Rafael Santos <rafael.santos@saedigital.com.br>
2018-09-24 23:39:50 -0300

    Inserido logo e alterado os menus para os corretos

---
+++ resources/views/dashboard/menu.blade.php
@@ -0,0 +1,29 @@
+<nav id="horizontal-nav" class="white hide-on-med-and-down">
+    <div class="nav-wrapper">
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li>
+                <a href="#!" class="cyan-text">
+                    <i class="mdi-action-dashboard"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="#!" class="cyan-text">
+                    <i class="mdi-communication-email"></i>
+                    <span>Medição</span>
+                </a>
+            </li>
+            <li>
+                <a class="dropdown-menu cyan-text" href="#!" data-activates="Reldropdown">
+                    <i class="mdi-action-invert-colors"></i>
+                    <span>Relatórios<i class="mdi-navigation-arrow-drop-down right"></i></span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<ul id="Reldropdown" class="dropdown-content dropdown-horizontal-list">
+    <li><a href="#!" class="cyan-text">Medições</a></li>
+    <li><a href="#!" class="cyan-text">Histórico</a></li>
+</ul>
---
+++ resources/views/dashboard/top.blade.php
@@ -0,0 +1,22 @@
+<nav class="cyan">
+    <div class="nav-wrapper">
+        <ul class="left">
+            <li class="no-hover"><a href="#" data-activates="slide-out" class="menu-sidebar-collapse btn-floating btn-flat btn-medium waves-effect waves-light cyan hide-on-large-only"><i class="mdi-navigation-menu" ></i></a></li>
+            <li><h1 class="logo-wrapper"><a href="#!" class="brand-logo darken-1"><img src="{{URL::asset('images/logo_glicon.png')}}" alt="glicon logo"></a> <span class="logo-text">Glicon</span></h1></li>
+        </ul>
+        <div class="header-search-wrapper hide-on-med-and-down">
+            <i class="mdi-action-search"></i>
+            <input type="text" name="Search" class="header-search-input z-depth-2" placeholder="Pesquisar"/>
+        </div>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="javascript:void(0);" class="waves-effect waves-block waves-light toggle-fullscreen"><i class="mdi-action-settings-overscan"></i></a>
+            </li>
+            <li><a href="javascript:void(0);" class="waves-effect waves-block waves-light"><i class="mdi-navigation-apps"></i></a>
+            </li>
+            <li><a href="javascript:void(0);" class="waves-effect waves-block waves-light"><i class="mdi-social-notifications"></i></a>
+            </li>
+            <li><a href="#" data-activates="chat-out" class="waves-effect waves-block waves-light chat-collapse"><i class="mdi-communication-chat"></i></a>
+            </li>
+        </ul>
+    </div>
+</nav>
